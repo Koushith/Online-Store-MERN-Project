6f4492e737120be630e17e4f24009bd733ffce44
koushith <koushith97@gmail.com>
2020-04-28 19:36:56 +0530

    custom middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
-const User = require("../models/user");
-const { check, validationResult } = require("express-validator");
-var jwt = require("jsonwebtoken");
-var expressJwt = require("express-jwt");
+const User = require('../models/user');
+const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
   const errors = validationResult(req);
@@ -16,7 +16,7 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: 'NOT able to save user in DB'
       });
     }
     res.json({
@@ -40,20 +40,20 @@ exports.signin = (req, res) => {
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "USER email does not exists"
+        error: 'USER email does not exists'
       });
     }
 
     if (!user.autheticate(password)) {
       return res.status(401).json({
-        error: "Email and password do not match"
+        error: 'Email and password do not match'
       });
     }
 
     //create token
     const token = jwt.sign({ _id: user._id }, process.env.SECRET);
     //put token in cookie
-    res.cookie("token", token, { expire: new Date() + 9999 });
+    res.cookie('token', token, { expire: new Date() + 9999 });
 
     //send response to front end
     const { _id, name, email, role } = user;
@@ -62,7 +62,38 @@ exports.signin = (req, res) => {
 };
 
 exports.signout = (req, res) => {
+  res.clearCookie('token'); //token--cookie name
   res.json({
-    message: "User signout"
+    message: 'User signout successfully'
   });
+};
+
+// Protected routes
+
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECERET,
+  userProperty: 'auth' //this can be anything
+});
+
+// custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  //next is required since it is middleware
+  // .profile is from frontend will define later
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id; //=== this is same id when we get while creating the user
+  if (!checker) {
+    return res.status(403).json({
+      error: 'Access Denied'
+    });
+  }
+  next();
+};
+
+// isAuthenenticated
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: 'you are not an Admin, Access Denied'
+    });
+  }
+  next();
 };
